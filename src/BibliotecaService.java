8d50f0830254bdc0d13567f3fa8637d818892ea1
alejandroparodcristian <115542447+alejandroparodcristian@users.noreply.github.com>
2024-10-22 08:58:33 +0200

    actulizar clase servicio

--- src/BibliotecaService.java
+++ src/BibliotecaService.java
@@ -1,16 +1,36 @@
 import java.util.ArrayList;
 
 public class BibliotecaService {
+    private DAOLibro libroDao;
     private UsuarioDAO usuarioDAO;
     private PrestamoDAO prestamoDAO;
     private ArrayList<Usuario> listaUsuario;
+    private ArrayList<Libro> listaLibro;
+    private ArrayList<Prestamo> listaPrestamo;
 
     public  BibliotecaService(){
         usuarioDAO = new UsuarioDAO();
     }
+
+    public void actualizarListaLibro(){
+        libroDao.leerLibros();
+
+    }
+
+    //Gestion Libro
+    public void leerLibro(){
+
+    }
+
+
     //Gestion Usuario
-    public void leerUsuario(){
+
+    public void actualizarListaUsuario(){
         listaUsuario = usuarioDAO.leerTodo();
+    }
+
+    public void leerUsuario(){
+        actualizarListaUsuario();
         for (Usuario u : listaUsuario){
             System.out.println(u);
         }
@@ -25,5 +45,16 @@ public class BibliotecaService {
         usuarioDAO.BorrarAlumno(id);
         listaUsuario.remove(id);
     }
-    //Gestion
+    //Gestion Préstamo
+
+    public void actualizarListaPrestamo(){
+        listaPrestamo = prestamoDAO.leerTodos();
+    }
+    public void leerTodos(){
+        for (Prestamo prestamo : listaPrestamo){
+            System.out.println(prestamo);
+        }
+    }
+
+
 }
